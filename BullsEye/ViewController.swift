f24e875bb5ad48612dbb6595b68013bd635fa8a4
Kirill Sofrin <sofrin2013@gmail.com>
2022-08-11 13:06:29 +0300

    Root commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Кирилл Софрин on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello world!", message: "Bulls eye app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+}
+
