87c11ed1b1b8ca9626414770a1057f7ee96b48d6
Kirill Sofrin <sofrin2013@gmail.com>
2022-08-11 17:04:22 +0300

    first beta witout score and rounds

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,19 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var targetValue = 0
 
+    var currentValue: Int = 0
+    
+    
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var targetLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
     
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello world!", message: "Bulls eye app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+    
+    @IBAction func showAlet(_ sender: UIButton) {
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is \(targetValue)"
+        let alert = UIAlertController(title: "Hello world!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
 
+    
 }
 
