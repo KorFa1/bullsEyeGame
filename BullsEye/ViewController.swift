09eff317712f891d766e41365876a08dde8dec0c
Kirill Sofrin <sofrin2013@gmail.com>
2022-08-17 11:21:00 +0300

    Completed project V 1.0

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -30,17 +30,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewGame()
-        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
-        slider.setThumbImage(thumbImageNormal, for: .normal)
-        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
-        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
-        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
-        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
-        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
-        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
-        let trackRightImage = UIImage(named: "SliderTrackRight")!
-        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
-        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        sliderAnimation()
     }
     
     
@@ -96,6 +86,25 @@ class ViewController: UIViewController {
         score = 0
         round = 0
         startNewRound()
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
+    }
+    
+    func sliderAnimation() {
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     
 }
